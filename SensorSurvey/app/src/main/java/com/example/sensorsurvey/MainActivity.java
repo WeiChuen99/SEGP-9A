546fa36cfe7407a279cade0a18e5e0f15516501d
KevinCSY <49639836+KevinCSY@users.noreply.github.com>
2019-10-17 12:58:36 +0800

    Accelerometer x y z values
    Program to retrieve accelerometer data from mobile device.

--- SensorSurvey/app/src/main/java/com/example/sensorsurvey/MainActivity.java
+++ SensorSurvey/app/src/main/java/com/example/sensorsurvey/MainActivity.java
@@ -26,16 +26,22 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE); // SensorManager to access device sensors
 
-        mTextSensorAccelerometer = (TextView) findViewById(R.id.label_accelerometer);
+        mTextSensorAccelerometer = (TextView) findViewById(R.id.label_accelerometer); // Access text in the app.
 
-        mSensorAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        mSensorAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION); // Get data from accelerometer
 
-        String sensor_error = getResources().getString(R.string.error_no_sensor);
+        /*  NOTE : It's using TYPE_LINEAR_ACCELERATION instead of TYPE_ACCELEROMETER
+        *   TYPE_ACCELEROMETER will add gravity into it's calculations which then allows to detect orientation
+        *   whereas TYPE_LINEAR_ACCELERATION will output raw acceleration data without calculating gravity.
+        *
+        *   Will need further discussion on which is more appropriate as the X Y Z values given might differ.
+        * */
 
+        String sensor_error = getResources().getString(R.string.error_no_sensor); // String variable
         if(mSensorAccelerometer == null) {
-            mTextSensorAccelerometer.setText(sensor_error);
+            mTextSensorAccelerometer.setText(sensor_error); // Print the string variable if no sensor is detected, e.g device doesn't have the sensor.
         }
 
     }
@@ -45,20 +51,20 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         super.onStart();
 
         if(mSensorAccelerometer != null){
-            mSensorManager.registerListener(this, mSensorAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+            mSensorManager.registerListener(this, mSensorAccelerometer, SensorManager.SENSOR_DELAY_NORMAL); // Listener to retrieve data
         }
     }
 
     @Override
     protected void onStop(){
         super.onStop();
-        mSensorManager.unregisterListener(this);
+        mSensorManager.unregisterListener(this); // Remove Listener
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
         int sensorType = event.sensor.getType();
-        float firstValue;
+        float firstValue; //variables to store data retrieved form sensor
         float secondValue;
         float thirdValue;
 
@@ -66,7 +72,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 firstValue = event.values[0];
                 secondValue = event.values[1];
                 thirdValue = event.values[2];
-                mTextSensorAccelerometer.setText(getResources().getString(R.string.label_accelerometer, firstValue, secondValue, thirdValue));
+                mTextSensorAccelerometer.setText(getResources().getString(R.string.label_accelerometer, firstValue, secondValue, thirdValue)); // Set the text in the app
 
         }
     }
